89baf7198093ff5e82b369e0d0b1397717482110
joaogkvalho <joaogkvalho@gmail.com>
2021-09-28 19:32:54 -0300

    challenge finished

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,36 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  return <></>
+  return(
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
+      <ModalOverlay />
+
+      <ModalContent
+        mx="auto"
+        w="auto"
+        h="auto"
+        maxWidth={['300px', '500px', '900px']}
+        maxHeight={['350px', '450px', '600px']}
+      >
+        <ModalBody p="0">
+          <Image 
+            src={imgUrl}
+            maxWidth={['300px', '500px', '900px']}
+            maxHeight={['350px', '450px', '600px']}
+          />
+        </ModalBody>
+
+        <ModalFooter
+          bg="pGray.800"
+          height="2rem"
+          py="20px"
+          borderBottomRadius="5px"
+        >
+          <Link href={imgUrl} isExternal fontSize="1rem" mr="auto">
+            Abrir Original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,15 +59,22 @@ export default function Home(): JSX.Element {
     return <Error />
   }
 
-  // TODO RENDER ERROR SCREEN
-
   return (
     <>
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        
+        {hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+            mt="6"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
